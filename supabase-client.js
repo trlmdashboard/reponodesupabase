57e8abe5fa3a422eeba28bd7d951b2353853545a
trlmdashboard <pm.mis.trlm@gmail.com>
2025-09-14 10:51:49 +0530

    Create supabase-client.js

---
+++ supabase-client.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
