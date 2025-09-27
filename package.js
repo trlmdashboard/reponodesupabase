5956f581244d436fa77e73482d7df9282238f534
trlmdashboard <pm.mis.trlm@gmail.com>
2025-09-28 01:50:18 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+    "name": "secure-supabase-login",
+    "version": "1.0.0",
+    "type": "module",
+    "scripts": {
+        "dev": "vercel dev"
+    },
+    "dependencies": {
+        "@supabase/supabase-js": "^2.38.0"
+    },
+    "devDependencies": {
+        "@vercel/node": "^2.9.10"
+    }
+}
